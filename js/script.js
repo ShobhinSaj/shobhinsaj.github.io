1f5e60c8b0c89d371a5efc736214412eb787c00c
madmax117 <maxjonas117@gmail.com>
2024-03-01 15:10:15 -0500

    Navbar width reponsive

--- js/script.js
+++ js/script.js
@@ -9,10 +9,12 @@ $(document).ready(function() {
       var breakpoint1 = 767;
         // Remove the class if the screen width is below the breakpoint
       if (screenWidth < breakpoint1) {
-        nav.removeClass('w-75');
+        nav.removeClass('w-75 rounded-4');
+        nav.addClass('rounded-bottom-4');
       } else {
         // Add the class back if the screen width is equal to or above the breakpoint
-        nav.addClass('w-75');
+        nav.removeClass('rounded-bottom-4');
+        nav.addClass('w-75 rounded-4');
       }
     }
   });
