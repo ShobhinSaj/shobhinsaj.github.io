f3060eb8979dc24ff9ae8b9bd6cad367e5ed8fde
madmax117 <maxjonas117@gmail.com>
2024-03-05 14:02:01 -0500

    homepage complete

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
 $(document).ready(function() {
-   
+    $('#downloadLink').on('click', function(e) {
+    // Trigger download
+      setTimeout(function() {
+        simulateDownload();
+    }, 1000); // Simulating a delay for the download to complete
+    });
+
+    // Show SweetAlert on successful download
+    function simulateDownload() {
+    Swal.fire({
+        icon: 'success',
+        title: 'Download Successful!',
+        showConfirmButton: false,
+        timer: 1500 // Adjust the timer as needed
+    });
+  }
     removeClassOnSmallScreen();
     $(window).resize(removeClassOnSmallScreen);
 
@@ -11,10 +26,13 @@ $(document).ready(function() {
       if (screenWidth < breakpoint1) {
         nav.removeClass('w-75 rounded-4');
         nav.addClass('rounded-bottom-4');
+        $('.hero_img').css("width", "80%");
+        $('.scrolldown').hide();
       } else {
         // Add the class back if the screen width is equal to or above the breakpoint
         nav.removeClass('rounded-bottom-4');
         nav.addClass('w-75 rounded-4');
+        $('.scrolldown').show();
       }
     }
   });
